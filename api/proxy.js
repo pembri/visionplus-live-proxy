ec4b508a7c8a0d5e1e52ead0aa1c778fe21e2c66
pembri <pembriahmad@gmail.com>
2026-06-11 04:07:50 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
     try {
         let targetUrl = null;
-        const path = req.query.path;
+        const path = req.query.path || '';
 
 
 const channelMap = {
